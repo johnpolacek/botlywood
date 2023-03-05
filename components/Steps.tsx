a6236713963c9c76f16a40388c796ca2fb262683
John Polacek <johnpolacek@gmail.com>
2023-03-05 16:28:20 -0600

    scroll to top on step change

--- components/Steps.tsx
+++ components/Steps.tsx
@@ -7,6 +7,13 @@ import StepTitle from "./StepTitle"
 const Steps: React.FC = () => {
   const { step } = useContext(AppContext)
 
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    })
+  }, [step])
+
   return (
     <div className="w-full relative z-10">
       <div className="h-full min-h-[50vw] flex flex-col items-center justify-center mx-auto relative z-10 pt-40 sm:pt-80">
